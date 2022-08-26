f87ddfeb10072028516ccbea54f7ba360f883df5
Sharaiz Ali <shahraiz.ali@crowdbotics.com>
2022-08-26 19:17:16 +0200

    Crowdbotics Studio: Update app screens

--- modules/app-menu/index.js
+++ modules/app-menu/index.js
@@ -5,63 +5,51 @@ import { useNavigation, useNavigationState } from "@react-navigation/native";
 
 function AppMenu() {
   const options = useContext(OptionsContext);
-  return (
-    <View style={options.styles.container}>
+  return <View style={options.styles.container}>
       <AppRoutes options={options} />
       <View style={options.styles.hr} />
       <GlobalOptions options={options} />
-    </View>
-  );
+    </View>;
 }
 
-function AppRoutes({ options }) {
+function AppRoutes({
+  options
+}) {
   const navigation = useNavigation();
-  const routes = useNavigationState((state) =>
-    state.routeNames.filter((name) => name !== options.title)
-  );
+  const routes = useNavigationState(state => state.routeNames.filter(name => name !== options.title));
 
-  const pressed = ({ pressed }) => [
-    pressed ? options.styles.buttonPressed : options.styles.buttonNotPressed,
-    options.styles.button
-  ];
-  const links = routes.map((route) => {
-    return (
-      <Pressable
-        onPress={() => navigation.navigate(route)}
-        style={pressed}
-        key={route}
-      >
+  const pressed = ({
+    pressed
+  }) => [pressed ? options.styles.buttonPressed : options.styles.buttonNotPressed, options.styles.button];
+
+  const links = routes.map(route => {
+    return <Pressable onPress={() => navigation.navigate(route)} style={pressed} key={route}>
         <Text style={options.styles.buttonText}>{route}</Text>
-      </Pressable>
-    );
+      </Pressable>;
   });
-  return (
-    <View>
+  return <View>
       <Text style={options.styles.text}>
         {options.copy}({routes.length})
       </Text>
       {links}
-    </View>
-  );
+    </View>;
 }
 
-function GlobalOptions({ options }) {
+function GlobalOptions({
+  options
+}) {
   const global = useContext(GlobalOptionsContext);
   const globalInfo = Object.entries(global).map(([key, val]) => {
-    return (
-      <Text style={options.styles.text} key={key}>
+    return <Text style={options.styles.text} key={key}>
         {key}: {val}
-      </Text>
-    );
+      </Text>;
   });
-  return (
-    <View>
+  return <View>
       <Text style={options.styles.text}>
         Global options({Object.entries(global).length})
       </Text>
       {globalInfo}
-    </View>
-  );
+    </View>;
 }
 
 export default {
